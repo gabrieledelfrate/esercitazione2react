a1ca464e9f8a02cb7de37d2e0ed374d72c359334
gabrieledelfrate <leledelfrate@outlook.com>
2023-12-19 17:10:16 +0100

    aggiunti tutti i libri da books.json

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -3,26 +3,29 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import booksData from '../data/scifi.json';
+import booksData from '../data/books.json';
 
 class AllTheBooks extends React.Component {
     render() {
       return (
         <div>
-          <Row xs={1} sm={2} md={3} lg={4}>
-            {booksData.map((book) => (
-              <Col key={book.asin}>
-                <Card style={{ width: '80%', margin: '2rem'}}>
-                  <Card.Img variant="top" src={book.img} />
-                  <Card.Body>
-                    <Card.Title>{book.title}</Card.Title>
-                    <Card.Text>Prezzo: {book.price}€</Card.Text>
-                    <Button variant="primary">Acquista subito</Button>
-                    <Button variant="secondary">Aggiungi al carrello</Button>
-                  </Card.Body>
-                </Card>
+         <Row xs={1} sm={2} md={3} lg={4}>
+              {Object.keys(booksData).map((category) => (
+                booksData[category].map((book, index) => (
+                  <Col key={index}>
+                    <Card style={{ width: '80%', margin: '2rem' }}>
+                      <Card.Img variant="top" src={book.img} />
+                      <Card.Body>
+                        <Card.Title>{book.title}</Card.Title>
+                        <Card.Text>Prezzo: {book.price}€</Card.Text>
+                        <Button variant="primary">Acquista subito</Button>
+                        <Button variant="secondary">Aggiungi al carrello</Button>
+                      </Card.Body>
+                    </Card>
+         
               </Col>
-            ))}
+            ))
+          ))}
           </Row>
         </div>
       );
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,3 +1,4 @@
+import './Welcome.css'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
